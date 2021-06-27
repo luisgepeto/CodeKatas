1e9877c1b7581bdc0e87692f8792ce7257faaa31
Luis Becerril <luisgepeto@users.noreply.github.com>
2021-06-27 13:36:56 -0500

    - Updated SpellChecker.Demo folder name and solution - Added basic options

---
+++ BloomFilters/src/SpellChecker.Demo/DemoOptions.cs
@@ -0,0 +1,21 @@
+﻿using CommandLine;
+
+namespace SpellChecker.Demo
+{
+    public class DemoOptions
+    {
+        [Option(Default = "en", HelpText = "Select the language for the spellchecker.")]
+        public string Language { get; set; }
+        [Option(HelpText = "A string to be spell checked.")]
+        public string Text { get; set; }
+        [Option(HelpText = "The file path to be spell checked")]
+        public string FilePath { get; set; }
+        public Language ParsedLanguage
+        {
+            get
+            {
+                return SpellChecker.Language.English;
+            }
+        }
+    }
+}
