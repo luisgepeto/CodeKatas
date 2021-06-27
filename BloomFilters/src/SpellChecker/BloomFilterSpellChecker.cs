d803757052a0ca090ab2932fd55efbc59859b4b6
Luis Becerril <luisgepeto@users.noreply.github.com>
2021-06-27 14:39:26 -0500

    Inherited BloomFilterSpellChecker from BaseSpellChecker

--- BloomFilters/src/SpellChecker/BloomFilterSpellChecker.cs
+++ BloomFilters/src/SpellChecker/BloomFilterSpellChecker.cs
@@ -4,14 +4,19 @@ using SpellChecker.Interfaces;
 
 namespace SpellChecker
 {
-    public class BloomFilterSpellChecker : ISpellChecker
+    public class BloomFilterSpellChecker : BaseSpellChecker, ISpellChecker
     {
-        public BloomFilterSpellChecker(BloomFilterSpellCheckerOptions options)
+        public BloomFilterSpellChecker(BloomFilterSpellCheckerOptions options) : base(options)
         {
-            
+
         }
 
         public Dictionary<string, bool> CheckByWords(string text)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void LoadWord(string word)
         {
             throw new System.NotImplementedException();
         }
